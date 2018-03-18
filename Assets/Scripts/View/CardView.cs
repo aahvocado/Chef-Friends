2fa9d4b119c8276f74c5fdf6dafa2257400f6bde
neonwednesdays <dxiao.ns@gmail.com>
2018-03-17 20:04:05 -0700

    onMouseOver card wip

--- Assets/Scripts/View/CardView.cs
+++ Assets/Scripts/View/CardView.cs
@@ -7,17 +7,33 @@ public class CardView : MonoBehaviour {
 	
 	private string id;
 	private TextMesh cardText;
+	private ParticleSystem SelectedParticleSystem;
 
 	// Use this for initialization
 	void Start () {
 		GameObject CardTextObject = this.transform.Find("CardText").gameObject;
 		cardText = CardTextObject.GetComponent(typeof(TextMesh)) as TextMesh;
 		cardText.text = displayText;
+
+		GameObject ParticleObject = this.transform.Find("CardSelectedParticles").gameObject;
+		SelectedParticleSystem = ParticleObject.GetComponent<ParticleSystem>();
+		var emission = SelectedParticleSystem.emission;
+		emission.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnMouseOver() {
+		var emission = SelectedParticleSystem.emission;
+		emission.enabled = true;
+	}
+
+	void OnMouseExit() {
+		var emission = SelectedParticleSystem.emission;
+		emission.enabled = false;
 	}
 
 	public void setDisplayText(string newText) {
